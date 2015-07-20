1146c9d6f50507afa08d4414671fcf5aa15fddba
Gustavo Saiani <gustavo@poe.ma>
2015-07-20 09:20:19 -0300

    Add Quantum package

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Dez.Mixfile do
   def application do
     [mod: {Dez, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :quantum]]
   end
 
   # Specifies which paths to compile per environment
@@ -34,6 +34,7 @@ defmodule Dez.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 1.1"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:quantum, ">= 1.3.0"}]
   end
 end

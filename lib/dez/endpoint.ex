3f63cbc01a45237df5f8cb92283e6ac57c36a85c
Gustavo Saiani <gustavo@poe.ma>
2015-08-02 10:15:46 -0300

    Add Favicon

--- lib/dez/endpoint.ex
+++ lib/dez/endpoint.ex
@@ -7,7 +7,7 @@ defmodule Dez.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :dez, gzip: false,
-    only: ~w(css images js favicon.ico robots.txt)
+    only: ~w(css images js favicon.png robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.

5c12203ae1b2cae0a9412d9e1806a33e0de1d027
Gustavo Saiani <gustavo@poe.ma>
2015-08-19 11:51:33 -0300

    Update Phoenix to 0.17

--- mix.exs
+++ mix.exs
@@ -35,11 +35,11 @@ defmodule Dez.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [
-      {:phoenix, "~> 0.16.1"},
+      {:phoenix, "~> 0.17.0"},
       {:phoenix_ecto, "~> 0.9"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.0"},
-      {:phoenix_live_reload, "~> 0.6", only: :dev},
+      {:phoenix_live_reload, "~> 1.0"},
       {:cowboy, "~> 1.0"},
       {:quantum, ">= 1.3.0"},
       {:httpoison, "~> 0.7"},

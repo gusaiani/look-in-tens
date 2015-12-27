9818ec8316f33977e3653777642ec25dde01222d
Gustavo Saiani <gustavo@poe.ma>
2015-12-26 22:49:25 -0200

    Move scrape functions to new module Scraper

--- web/controllers/company_controller.ex
+++ web/controllers/company_controller.ex
@@ -5,19 +5,6 @@ defmodule Dez.CompanyController do
 
   plug :scrub_params, "company" when action in [:create, :update]
 
-  def scrape do
-    url = "https://raw.githubusercontent.com/matthewfieger/bloomberg_stock_data/master/tickers/nyse.csv"
-
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        parse body
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
-    end
-  end
-
   def index(conn, _params) do
     companies = Repo.all(Company)
     render(conn, "index.html", companies: companies)
@@ -76,32 +63,4 @@ defmodule Dez.CompanyController do
     |> put_flash(:info, "Company deleted successfully.")
     |> redirect(to: company_path(conn, :index))
   end
-
-  defp parse(body) do
-    {:ok, table} = body
-      |> ExCsv.parse(headings: true)
-
-    IO.inspect table.headings
-    add table.body
-  end
-
-  defp add([]) do
-    IO.puts "Finished"
-  end
-
-  defp add([head|tail]) do
-    name   = Enum.at(head, 1)
-    ticker = Enum.at(head, 0)
-    pe = Float.floor(:random.uniform * 20, 2)
-
-    changeset = Company.changeset(%Company{}, %{"name" => name, "ticker" => ticker, "pe" => pe})
-
-    if changeset.valid? do
-      Repo.insert!(changeset)
-      IO.puts "New company added: #{name}!"
-    end
-
-    add tail
-  end
-
 end

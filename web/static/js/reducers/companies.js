779fe386331e150f78576ffc9ea3c73865af29d8
Gustavo Saiani <gustavo@poe.ma>
2015-12-29 22:01:35 -0200

    Basic redux setup

---
+++ web/static/js/reducers/companies.js
@@ -0,0 +1,7 @@
+const initialState = {
+  companies: {}
+}
+
+export default function companies(state = initialState, action) {
+  return state
+}
---
+++ web/static/js/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import companies from './companies'
+
+const rootReducer = combineReducers({
+  companies
+})
+
+export default rootReducer

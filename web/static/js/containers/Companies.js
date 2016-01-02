8f6ef83ef059175681145b9655fb51e660948b5a
Gustavo Saiani <gustavo@poe.ma>
2016-01-02 19:42:51 -0200

    Install package

--- web/static/js/containers/Companies.js
+++ web/static/js/containers/Companies.js
@@ -1,9 +1,55 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import Autowhatever from 'react-autowhatever'
+import { updateInputValue } from '../actions/index'
+
+const items = [{
+  text: 'Apple'
+}, {
+  text: 'Banana'
+}, {
+  text: 'Cherry'
+}, {
+  text: 'Grapefruit'
+}, {
+  text: 'Lemon'
+}]
 
 class Companies extends Component {
+  renderItem(item) {
+    return (
+      <span>{item.text}</span>
+    )
+  }
+
   render() {
-    return <h1>We’ve got companies’ view</h1>
+    const { value, focusedSectionIndex, focusedItemIndex, onChange, onKeyDown } = this.props;
+    const inputProps = { value, onChange, onKeyDown }
+
+    return (
+      <div>
+        <h1>We’ve got companies’ view</h1>
+          <Autowhatever
+            id="0"
+            items={items}
+            renderItem={this.renderItem}
+            inputProps={inputProps}
+            focusedSectionIndex={focusedSectionIndex}
+            focusedItemIndex={focusedItemIndex} />
+      </div>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onChange: event => dispatch(updateInputValue(exampleId, event.target.value)),
+    onKeyDown: (event, { newFocusedSectionIndex, newFocusedItemIndex }) => {
+      if (typeof newFocusedItemIndex !== 'undefined') {
+        event.preventDefault()
+        dispatch(updateFocusedItem(exampleId, newFocusedSectionIndex, newFocusedItemIndex))
+      }
+    }
   }
 }
 
@@ -11,4 +57,4 @@ function mapStateToProps(state) {
   return {}
 }
 
-export default connect(mapStateToProps)(Companies)
+export default connect(mapStateToProps, mapDispatchToProps)(Companies)
--- web/static/js/reducers/companies.js
+++ web/static/js/reducers/companies.js
@@ -1,7 +1,27 @@
-const initialState = {
-  companies: {}
-}
+import { UPDATE_INPUT_VALUE, UPDATE_FOCUSED_ITEM } from '../constants/actionTypes';
 
-export default function companies(state = initialState, action) {
-  return state
+export default function companies (state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_INPUT_VALUE:
+      return {
+        ...state,
+        [action.exampleNumber]: {
+          ...state[action.exampleNumber],
+          value: action.value
+        }
+      }
+
+    case UPDATE_FOCUSED_ITEM:
+      return {
+        ...state,
+        [action.exampleNumber]: {
+          ...state[action.exampleNumber],
+          focusedSectionIndex: action.focusedSectionIndex,
+          focusedItemIndex: action.focusedItemIndex
+        }
+      }
+
+    default:
+      return state
+  }
 }

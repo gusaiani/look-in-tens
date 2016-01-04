a639edc92286162e71608bcdec89b596de891fd3
Gustavo Saiani <gustavo@poe.ma>
2016-01-04 18:20:45 -0200

    Basic company action with thunk

--- web/static/js/actions/companies.js
+++ web/static/js/actions/companies.js
@@ -1,5 +1,11 @@
 import { UPDATE_INPUT_VALUE, UPDATE_FOCUSED_ITEM } from '../constants/actionTypes';
 
+export function searchCompany(queryStr) {
+  return (dispatch) => {
+    console.log('Nesta bosta', queryStr)
+  }
+}
+
 export function updateInputValue(exampleNumber, value) {
   return {
     type: UPDATE_INPUT_VALUE,

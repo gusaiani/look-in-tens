1ba87d9c9bf56bb823f7cc1d91c3d15716799786
Gustavo Saiani <gustavo@poe.ma>
2016-01-11 14:10:56 -0200

    Add lookintens as domain in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :dez, Dez.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "lookintens.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support

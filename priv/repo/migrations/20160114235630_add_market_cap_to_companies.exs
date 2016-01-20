a94dbd141a53881ce45fede9c9425feefc98da5c
Gustavo Saiani <gustavo@poe.ma>
2016-01-20 15:50:42 -0200

    Fetcher for companies’ market caps

--- priv/repo/migrations/20160114235630_add_market_cap_to_companies.exs
+++
@@ -1,9 +0,0 @@
-defmodule Dez.Repo.Migrations.AddMarketCapToCompanies do
-  use Ecto.Migration
-
-  def change do
-    alter table(:companies) do
-      add :market_cap, :string
-    end
-  end
-end

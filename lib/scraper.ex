4a467a625c929386c3cd7a82b79d0b4abef5c4b7
Gustavo Saiani <gustavo@poe.ma>
2016-01-20 21:46:17 -0200

    Improve Scraper clarity

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -1,31 +1,16 @@
 defmodule Scraper do
-  use Application
-
-  def start(_type, _args) do
-    Dez.Supervisor.start_link()
-  end
-
-  def start_link(backend, query, query_ref, owner, limit) do
-    backend.start_link(query, query_ref, owner, limit)
-  end
+  alias Dez.{Company, Repo}
 
   def scrape do
-    scrape_stock_exchange(exchanges)
+    for exchange <- exchanges do
+      get_companies_from_stock_exchange(exchange)
+    end
   end
 
-  defp scrape_stock_exchange([head|tail]) do
-    get_companies_from_stock_exchange(head)
-    scrape_stock_exchange(tail)
-  end
-
-  defp scrape_stock_exchange([]) do
-    IO.puts "Exchanges scraped."
-  end
-
-  defp get_companies_from_stock_exchange({exchange, url}) do
+  defp get_companies_from_stock_exchange({_exchange, url}) do
     case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        parse body
+      {:ok, %HTTPoison.Response{status_code: 200, body: companies}} ->
+        import_companies(companies)
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "Not found :("
       {:error, %HTTPoison.Error{reason: reason}} ->
@@ -33,56 +18,50 @@ defmodule Scraper do
     end
   end
 
-  defp parse(body) do
-    {:ok, table} = body
-      |> ExCsv.parse(headings: true)
+  def import_companies(companies) do
+    companies
+    |> parse_list
+    |> add
+  end
 
-    IO.inspect table.headings
-    add table.body
+  defp parse_list(companies) do
+    {:ok, table} = companies |> ExCsv.parse(headings: true)
+    table.body
   end
 
   defp add([]) do
     IO.puts "Stock exchange scrape finished."
   end
 
-  alias Dez.{Company, Repo}
-
   defp add([company|tail]) do
     ticker = Enum.at(company, 0)
     market_cap = MarketCap.scrape(ticker)
 
     case market_cap do
-      {:ok, market_cap} ->
-        name = Enum.at(company, 1)
-        pe = Float.floor(:random.uniform * 20, 2)
-
-        changeset = Company.changeset(%Company{}, %{
-          "name" => name,
-          "ticker" => ticker,
-          "pe" => pe,
-          "market_cap" => market_cap})
-
-          case Repo.insert(changeset) do
-            {:ok, _company} ->
-              IO.puts "New company added: #{name}"
-            {:error, changeset} ->
-              IO.inspect "Error saving #{name} to database."
-          end
+      {:ok, market_cap} -> save(company, market_cap)
     end
 
     add tail
   end
 
-  defp await_results(children, opts) do
-    timeout = opts[:timeout] || 5000
-    timer = Process.send_after(self(), :timedout, timeout)
-    results = await_result(children, [], :infinity)
-    cleanup(timer)
-    results
-  end
+  defp save(company, market_cap) do
+    IO.inspect(company)
+    name   = Enum.at(company, 1)
+    ticker = Enum.at(company, 0)
+    pe     = Float.floor(:random.uniform * 20, 2)
 
-  defp await_result do
+    changeset = Company.changeset(%Company{}, %{
+      "name" => name,
+      "ticker" => ticker,
+      "pe" => pe,
+      "market_cap" => market_cap})
 
+      case Repo.insert(changeset) do
+        {:ok, _company} ->
+          IO.puts "New company added: #{name}"
+        {:error, _changeset} ->
+          IO.inspect "Error saving #{name} to database."
+      end
   end
 
   defp exchanges do

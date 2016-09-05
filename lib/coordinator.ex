cdbb6d2c188cd0870e19a08f3cc94a63d6ada31a
Gustavo Saiani <gustavo@poe.ma>
2016-09-05 10:24:05 -0300

    Create or update when finding ticker and mkt cap

--- lib/coordinator.ex
+++ lib/coordinator.ex
@@ -1,5 +1,6 @@
 defmodule Dez.Coordinator do
-  alias Dez.{Company, Repo, Scraper.NetIncome}
+  alias Dez.{Company, Repo}
+  import Ecto.Query
 
   def loop(results \\ [], results_expected) do
     receive do
@@ -10,47 +11,62 @@ defmodule Dez.Coordinator do
 
         save(company, market_cap)
 
-        # worker = Task.async(NetIncome, :fetch, [ticker])
-        # net_income = Task.await(worker)
-
-        # case net_income do
-        #   :error -> IO.puts "Error retrieving net income for #{ticker}"
-        #   _      -> save(company, market_cap, net_income)
-        # end
-
         if results_expected == Enum.count(new_results) do
           send self, :exit
         end
 
         loop(new_results, results_expected)
+
       :exit ->
-        IO.puts("Scrape finished")
+        IO.puts "Scrape finished"
+
       _ ->
         loop(results, results_expected)
     end
   end
 
-  # defp save(company, market_cap, net_income) do
   defp save(company, market_cap) do
     name = Enum.at(company, 1)
     ticker = Enum.at(company, 0)
-    pe10 = "5000"
 
-    changeset = Company.changeset(%Company{}, %{
+    new_company = %{
       "name" => name,
       "ticker" => ticker,
-      "pe10" => pe10,
-      "market_cap" => market_cap,
-      # "net_income" => net_income})
-      "net_income" => "3000"})
+      "market_cap" => market_cap}
+
+    query = Company |> where(ticker: ^ticker)
+
+    case Repo.one(query) do
+      nil     -> create(new_company)
+      company -> edit(company, new_company)
+    end
+  end
+
+  defp create(company) do
+    changeset = Company.changeset(%Company{}, company)
 
     case Repo.insert(changeset) do
-      {:ok, _company} ->
-        IO.puts "New company added: #{name}"
-        ticker
-      {:error, _changeset} ->
-        IO.inspect changeset.errors
-        IO.inspect "Error saving #{name} to database."
+        {:ok, _} ->
+          IO.puts "Company created: #{company["name"]}"
+          company["ticker"]
+        {:error, changeset} ->
+          IO.inspect changeset.errors
+          IO.inspect "Error creating #{company["name"]}"
+          :error
+    end
+  end
+
+  defp edit(company, changes) do
+    changeset = Ecto.Changeset.change(company, changes)
+
+    case Repo.update(changeset) do
+        {:ok, _} ->
+          IO.puts "Company updated: #{changes["name"]}"
+          changes["ticker"]
+        {:error, changeset} ->
+          IO.inspect "Error updating #{changes["name"]}"
+          IO.inspect changeset.errors
+          :error
     end
   end
 end
